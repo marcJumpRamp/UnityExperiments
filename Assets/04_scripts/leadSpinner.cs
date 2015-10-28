206efdd8918ef798fd130fa02d9d494206391d1e
marc <marc@jumprampgames.com>
2015-10-28 11:59:48 -0400

    First push of 3D menu system test.

---
+++ Assets/04_scripts/leadSpinner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class leadSpinner : MonoBehaviour {
+
+    public Quaternion turnBoard;        //Rotation variable
+    public float turnAmount = 0.0f;     //Turn radius variable
+	
+
+	void Update () {
+        turnBoard = Quaternion.Euler(0, turnAmount, 0);     //Set up the rotation
+        transform.rotation = turnBoard;     //Rotate the object
+        if (Input.GetKeyUp("z")) {
+            turnAmount -= 60.0f;        //Turn to the left
+        }
+        if (Input.GetKeyUp("x")) {
+            turnAmount += 60.0f;        //Turn to the right
+        }
+    }
+}
---
+++ Assets/04_scripts/turnMenuBoard.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class turnMenuBoard : MonoBehaviour {
+    
+    public Transform startHere;     //Put the local spin axis here
+    public Transform endHere;       //Put the spin target here
+    public float turnSpeed = 0.5f;      //Speed of spin
+
+	void Update () {
+        transform.rotation = Quaternion.Slerp(startHere.rotation, endHere.rotation, turnSpeed);     //Smoothly follow the spin of the target object
+    }
+}
